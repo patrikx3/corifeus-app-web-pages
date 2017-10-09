99e16e94ffcb9e2e4f238a7975fc62c795a475f2
patrikx3 <alabard@gmail.com>
2017-10-09 12:27:58 +0200

    p3x-robot sunday release 2017-10-9 12:27:58

--- src/angular/component/cory-web-pages-build-status.ts
+++ src/angular/component/cory-web-pages-build-status.ts
@@ -21,6 +21,8 @@ import {LocaleService, LocaleSubject} from "corifeus-web";
     <a target="_blank" href="https://scrutinizer-ci.com/g/patrikx3/{{ repo}}/?branch=master"><img src="https://scrutinizer-ci.com/g/patrikx3/{{ repo}}/badges/quality-score.png?b=master" [matTooltip]="i18n.pages.title.scrunitizer.quality" [matTooltipPosition]="tooltipPosition"></a>
     
     <a target="_blank" href="https://scrutinizer-ci.com/g/patrikx3/{{ repo}}/?branch=master"><img src="https://scrutinizer-ci.com/g/patrikx3/{{ repo}}/badges/coverage.png?b=master"  [matTooltip]="i18n.pages.title.scrunitizer.coverage" [matTooltipPosition]="tooltipPosition"></a>
+    
+     <a target="_blank" href="https://www.facebook.com/corifeus.software"><img src="https://img.shields.io/badge/LIKE-Corifeus-3b5998.svg"  matTooltip="Corifeus Software Engineering" [matTooltipPosition]="tooltipPosition"></a>
 </span>
 `
 })
